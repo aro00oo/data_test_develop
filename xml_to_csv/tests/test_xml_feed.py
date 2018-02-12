82276822f967478e5bdf72f849d9d31d4a22dbfe
aro00oo <aro00oo@yahoo.com>
2018-02-11 18:31:52 -0700

    Feed class for normalizing inputs, unittest

---
+++ xml_to_csv/tests/test_xml_feed.py
@@ -0,0 +1,68 @@
+from mock import patch, MagicMock, PropertyMock, Mock
+from unittest import TestCase, main
+
+from ..xml_feed import XmlFeed
+
+
+class TestEnter(TestCase):
+    """Various cases for the enter method"""
+
+    @patch('xml_to_csv.xml_feed.requests')
+    def test_enter_with_url(self, requestsMock):
+        """Ensure requests and response.content gets called"""
+        TEST_URL = "http://test.com"
+        getMethod = MagicMock()
+        getMethodReturnValue = Mock()
+        getMethod.return_value = getMethodReturnValue
+        contentPropertyMock = PropertyMock()
+        type(getMethodReturnValue).content = contentPropertyMock
+        requestsMock.get = getMethod
+        with XmlFeed(TEST_URL) as feed:
+            self.assertTrue(feed.is_url)
+            self.assertFalse(feed.is_file)
+            pass
+        getMethod.assert_called_with(TEST_URL)
+        contentPropertyMock.assert_called()
+
+    @patch('xml_to_csv.xml_feed.open')
+    @patch('xml_to_csv.xml_feed.os')
+    def test_enter_with_file(self, osMock, openMock):
+        """Ensure file is opened"""
+        TEST_FILE = '/test/testfile.test'
+        FILE_MODE = 'rb'
+        with XmlFeed(TEST_FILE) as feed:
+            self.assertTrue(feed.is_file)
+            self.assertFalse(feed.is_url)
+            pass
+
+        openMock.assert_called_with(TEST_FILE, FILE_MODE)
+
+    def test_enter_with_string(self):
+        TEST_INPUT_STR = "<body><h1>hello world</h1></body>"
+        with XmlFeed(TEST_INPUT_STR) as feed:
+            self.assertFalse(feed.is_file)
+            self.assertFalse(feed.is_url)
+
+
+class TestExit(TestCase):
+    """Case for the exit method"""
+
+    #Only testing with file as that's the only interesting portion implemented
+    #One might argue still make explicit tests for it given the enter method above, but /shrug
+
+    @patch('xml_to_csv.xml_feed.open')
+    @patch('xml_to_csv.xml_feed.os')
+    def test_exit_with_file(self, osMock, openMock):
+        """Ensure file is closed"""
+        TEST_FILE = '/test/testfile.test'
+
+        openMockReturnValue = Mock()
+        closeMethodMock = MagicMock()
+        openMockReturnValue.close = closeMethodMock
+        openMock.return_value = openMockReturnValue
+
+        with XmlFeed(TEST_FILE) as feed:
+            self.assertTrue(feed.is_file)
+            self.assertFalse(feed.is_url)
+            pass
+        closeMethodMock.assert_called()
---
+++ xml_to_csv/xml_feed.py
@@ -0,0 +1,40 @@
+import os
+import requests
+
+from urlparse import urlparse
+
+
+class XmlFeed:
+    """Represents XML in some type of feed"""
+    def __init__(self, input):
+        self.input = input
+
+    def __enter__(self, *args, **kwargs):
+        """Initialize the input as a feed"""
+        if self.is_url:
+            self.feed = requests.get(self.input).content
+        elif self.is_file:
+            self.feed = open(self.input, 'rb')
+        elif isinstance(self.input, str):
+            self.feed = self.input
+        else:
+            raise TypeError("Please provide a parse-able input [file/url/text].")
+
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        """Close the feed if this is a file"""
+        if self.is_file:
+            self.feed.close()
+        return self
+
+    @property
+    def is_url(self):
+        """The input is a URL type"""
+        parsedUrlResult = urlparse(self.input)
+        return bool(parsedUrlResult.scheme and parsedUrlResult.netloc)
+
+    @property
+    def is_file(self):
+        """The input is a file"""
+        return os.path.isfile(self.input)
